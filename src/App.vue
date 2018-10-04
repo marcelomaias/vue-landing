44511248b7267417071ada340ced87ef7fcb2f3c
Maia <maia@Citadel.local>
2018-10-04 04:51:17 -0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app v-scroll="onScroll">
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in menuItems"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed flat dark color="transparent">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      {{ offsetTop }}
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat v-for="(item, i) in menuItems" :key="i">
+          <v-icon left dark v-html="item.icon"></v-icon>
+          {{ item.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      offsetTop: 0,
+      scrolling: false,
+      menuItems: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      title: 'Awesome Landing'
+    }
+  },
+  methods: {
+    onScroll () {
+      this.offsetTop = window.pageYOffset || document.documentElement.scrollTop
+      // if(this.offsetTop > 100) {
+      //   scrolling: true
+      // }
     }
   }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Hero />
+    <v-container>
+      <v-slide-y-transition mode="out-in">
+        <v-layout column align-center>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio cumque error inventore rerum aperiam ipsa atque asperiores hic temporibus deserunt similique tempora facere iure minima, debitis ab modi laborum. Nemo harum dolores debitis recusandae odio, maiores nostrum molestiae vero fugiat rem consequatur assumenda ducimus quibusdam mollitia doloremque at, quam qui cum accusamus illo. Ducimus illum sed iusto doloribus rem quos veritatis nostrum aut tenetur voluptas eaque repellat iste reiciendis neque repudiandae, asperiores magni iure quasi quod rerum dolore cumque eveniet labore adipisci! Facilis tempora molestias in minus expedita magnam, ad iste, repellat aut voluptatibus voluptates dolorum laudantium accusamus sint dicta!</p>
+        </v-layout>
+      </v-slide-y-transition>
+    </v-container>
+
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Hero from '../components/Hero.vue'
 
 export default {
-  name: 'home',
+  data () {
+    return {
+      offsetTop: 0
+    }
+  },
   components: {
-    HelloWorld
+    Hero
   }
 }
 </script>
+
