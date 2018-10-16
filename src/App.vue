0de0cde67b0b082a160458894abc095ea674e50c
Maia <maia@Citadel.local>
2018-10-16 09:07:00 -0300

    Navigation Fixed

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,46 @@
   <v-app v-scroll="onScroll">
     <v-navigation-drawer temporary v-model="drawer" enable-resize-watcher fixed app>
       <v-list>
-        <v-list-tile value="true" v-for="(link, i) in menuLinks" :key="i" :href="link.target">
+        <v-list-tile @click="$vuetify.goTo('#what')">
+          <v-list-tile-action>
+            <v-icon class="black--text">build</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="black--text">What we do</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="$vuetify.goTo('#process')">
+          <v-list-tile-action>
+            <v-icon class="black--text">view_list</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="black--text">Work Process</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="$vuetify.goTo('#projects')">
+          <v-list-tile-action>
+            <v-icon class="black--text">bubble_chart</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="black--text">Projects</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="$vuetify.goTo('#contact')">
+          <v-list-tile-action>
+            <v-icon class="black--text">chat_bubble</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="black--text">Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <!-- <v-list-tile value="true" v-for="(link, i) in menuLinks" :key="i" :href="link.target">
           <v-list-tile-action>
             <v-icon v-html="link.icon" class="black--text"></v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title v-text="link.title" class="black--text"></v-list-tile-title>
           </v-list-tile-content>
-        </v-list-tile>
+        </v-list-tile> -->
       </v-list>
     </v-navigation-drawer>
     <v-toolbar fixed :flat="!isScrolling" :class="{ transparent: !isScrolling }">
@@ -21,11 +53,14 @@
       <v-spacer></v-spacer>
       <!-- {{ offsetTop }} -->
       <v-toolbar-items class="hidden-sm-and-down">
-        <!-- <v-btn flat v-for="(link, i) in menuLinks" :key="i" @click="$vuetify.goTo(target)"> -->
-        <v-btn flat v-for="(link, i) in menuLinks" :key="i" :href="link.target">
+        <v-btn flat @click="$vuetify.goTo('#what')"><v-icon left dark>build</v-icon>What we do</v-btn>
+        <v-btn flat @click="$vuetify.goTo('#process')"><v-icon left dark>view_list</v-icon>Work Process</v-btn>
+        <v-btn flat @click="$vuetify.goTo('#projects')"><v-icon left dark>bubble_chart</v-icon>Projects</v-btn>
+        <v-btn flat @click="$vuetify.goTo('#contact')"><v-icon left dark>chat_bubble</v-icon>Contact</v-btn>
+        <!-- <v-btn flat v-for="(link, i) in menuLinks" :key="i" :href="link.target">
           <v-icon left dark v-html="link.icon"></v-icon>
           {{ link.title}}
-        </v-btn>
+        </v-btn> -->
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@
     <v-layout align-center column justify-center black--text>
       <div class="text-uppercase">Agency / Corporate / Freelancer / Portfolio</div>
       <h1 class="display-3 text-uppercase font-weight-black my-2 titleAnim">Make things Happen</h1>
-      <v-btn large dark href="#what">Learn More</v-btn>
+      <v-btn large dark @click="$vuetify.goTo('#what')">Learn More</v-btn>
     </v-layout>
   </v-parallax>
 </template>
